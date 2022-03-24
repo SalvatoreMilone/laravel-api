159fdc6cafd37ec20077f29ecc20be101b89043c
SalvatoreMilone <Salvatore.Milone@outlook.it>
2022-03-24 17:33:25 +0100

    vue - api

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/posts", "Api\PostController@index");
 Route::get("/posts/category/{category}", "Api\PostController@index");
 Route::get("/posts/{id}/{category}", "Api\PostController@filter");
+
+// Route::namespace('Api')->name('api')->group(function() {
+//     Route::get("/posts", "PostController@index")->name('posts');
+// });
